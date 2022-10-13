56d53f4595c78cf3f62d9ef9b50167f431d596fd
Trista <starry1735@gmail.com>
2022-10-13 17:17:00 +0800

    20221013 17:16 Trista前台完成串接+CRUD

---
+++ src/main/java/tw/trista/flightticket/model/flightticketorderRepository.java
@@ -0,0 +1,7 @@
+package tw.trista.flightticket.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface flightticketorderRepository extends JpaRepository<flightticketorder, String> {
+
+}
